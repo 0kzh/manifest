178637874ca113d9f074dd7cc27bb53dc3779d05
Kelvin Zhang <me@kelvinzhang.ca>
2021-03-28 17:58:18 -0700

    Add datepicker component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, forwardRef } from "react";
 import Calendar from "./components/calendar/Calendar";
+import DatePicker from "react-datepicker";
 import { formatDate } from "./util/helper";
 import { DocumentTextOutline, ChevronLeftOutline, ChevronRightOutline }from "heroicons-react";
 import './App.css';
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
   const [date, setDate] = useState<Date>(new Date());
@@ -19,6 +21,18 @@ function App() {
     setDate(newDate)
   }
 
+  interface IPropInput {
+    onClick: (date: any) => void;
+  }
+
+  const DateDisplay = forwardRef(
+    ({ onClick }: IPropInput, ref: React.LegacyRef<HTMLDivElement>) => (
+      <div className="date-display" onClick={onClick} ref={ref}>
+        <b>{formatDate(date)}</b>
+      </div>
+    )
+  );
+
   return (
     <div className="container">
       <div className="header">
@@ -28,7 +42,13 @@ function App() {
           </div>
           <div className="flex-row vert-center" style={{ gap: 5 }}>
             <ChevronLeftOutline onClick={prevDay} className="icon-circular caret" />
-            <b>{formatDate(date)}</b>
+            <div>
+              <DatePicker
+                selected={date}
+                onChange={(date: Date) => setDate(date)} 
+                customInput={<DateDisplay onClick={() => {}} />}
+              />
+            </div>
             <ChevronRightOutline onClick={nextDay} className="icon-circular caret" />
           </div>
       </div>
