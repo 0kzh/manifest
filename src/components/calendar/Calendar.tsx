2ed67f125e9fa0c5b0041c2b74bcf4eeea6c5fa5
Kelvin Zhang <me@kelvinzhang.ca>
2022-08-27 22:34:35 -0700

    conditionally render fill from yesterday on calendar

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -129,7 +129,7 @@ const Calendar: React.FC<Props> = (props: Props) => {
             />
           ))}
       <TimeIndicator startTime={startTime} endTime={endTime} />
-      {events.length === 0 && <button
+      {events.length === 0 && yesterdaysEvents.length > 0 && <button
         className="flex items-center justify-center gap-2 rounded bg-gray-100 hover:bg-gray-200 text-gray-500 w-full mt-2 py-2"
         onClick={fillFromYesterday}
       >
